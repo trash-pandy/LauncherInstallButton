0f5daea6a12f4e71fd0dcf19e011cd9b6a30df42
trash-pandy <evancalderon15@gmail.com>
2023-04-20 17:17:16 -0700

    Check if the website is actually modrinth...

--- src/background.ts
+++ src/background.ts
@@ -2,9 +2,14 @@ import browser from "webextension-polyfill";
 
 browser.action.onClicked.addListener(async (event) => {
   if (event.url == undefined) return;
-  let url = new URL(event.url);
-  let path = url.pathname.split("/").filter((s) => s.length !== 0);
 
+  let url = new URL(event.url);
+  let hostname = url.hostname.split(".");
+  if (hostname.at(-2) !== "modrinth" || hostname.at(-1) !== "com") {
+    return;
+  }
+
+  let path = url.pathname.split("/").filter((s) => s.length !== 0);
   if (!path[0].endsWith("s") && path[1] !== undefined) {
     window.location.assign(`modrinth-mc://${path[0]}/${path[1]}`);
   }
