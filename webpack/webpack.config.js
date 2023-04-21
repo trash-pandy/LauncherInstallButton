716f86fe257d77f23bbb446889875f8aaf568b23
trash-pandy <evancalderon15@gmail.com>
2023-04-20 18:00:37 -0700

    Finalize extension building

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
+const ZipPlugin = require("zip-webpack-plugin");
 module.exports = {
   mode: "production",
   entry: {
@@ -24,6 +25,11 @@ module.exports = {
   plugins: [
     new CopyPlugin({
       patterns: [{ from: ".", to: ".", context: "public" }],
+    }),
+    new ZipPlugin({
+      filename: "launcher-install-button",
+      extension: "xpi",
+      path: "../build",
     }),
   ],
 };
